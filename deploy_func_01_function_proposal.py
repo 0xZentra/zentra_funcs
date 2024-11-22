af36b1fc37e68667a4a1d6b94706eab11eff3e74
KJ <kernel1983@gmail.com>
2024-11-21 21:06:19 -0500

    the fundamental funcs

---
+++ deploy_func_01_function_proposal.py
@@ -0,0 +1,106 @@
+
+import hashlib
+import json
+
+import web3
+
+
+PROVIDER_HOST = 'http://127.0.0.1:8545'
+
+w3 = web3.Web3(web3.Web3.HTTPProvider(PROVIDER_HOST))
+
+function_proposal_sourcecode = '''
+def function_proposal(info, args):
+    assert args['f'] == 'function_proposal'
+    sender = info['sender'].lower()
+    handle = handle_lookup(sender)
+    fname = args['a'][0]
+    assert set(fname) <= set(string.ascii_lowercase+'_')
+    sourcecode = args['a'][1]
+
+    require = args['a'][2]
+    for i in require:
+        assert type(i) is list
+        assert set(i[0]) <= set(string.ascii_lowercase+'_')
+        assert type(i[1]) is list
+        for j in i[1]:
+            assert set(j) <= set(string.ascii_uppercase+'_')
+
+    asset_permission = args['a'][3]
+    assert type(asset_permission) is list
+    for i in asset_permission:
+        assert i == '*' or set(i) <= set(string.ascii_lowercase+'_')
+
+    invoke_permission = args['a'][4]
+    assert type(invoke_permission) is list
+    for i in invoke_permission:
+        assert i == '*' or set(i) <= set(string.ascii_lowercase+'_')
+
+    hexdigest = hashlib.sha256(sourcecode.encode('utf8')).hexdigest()
+    k = 'function-proposal-%s:%s' % (fname, hexdigest)
+    put(handle, 'function', 'proposal', {'sourcecode': sourcecode, 'asset_permission': asset_permission, 'require': require, 'votes': []}, '%s:%s' % (fname, hexdigest))
+'''
+
+
+if __name__ == '__main__':
+    # committee_init will only be called once and not in global state
+    # function_proposal in global state
+    # function_vote in global state
+
+    account = setting.account
+    nonce = w3.eth.get_transaction_count(account.address)
+    print(account.address, nonce)
+
+    # call committee_init
+    call = {"p":"minus", 'f': 'committee_init', 'a': []}
+    transaction = {
+        'from': account.address,
+        'to': account.address,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 0,
+        'maxFeePerGas': 0,
+        'maxPriorityFeePerGas': 0,
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    print(tx_hash.hex())
+
+    call = {"p": "minus",
+            "f": "function_proposal",
+            "a": ['function_proposal', function_proposal_sourcecode, [], [], []]}
+    transaction = {
+        'from': account.address,
+        'to': account.address,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 0,
+        'maxFeePerGas': 0,
+        'maxPriorityFeePerGas': 0,
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    print(tx_hash.hex())
+
+
+    call = {"p": "minus",
+            "f": "function_vote", 
+            "a": ['function_proposal', hashlib.sha256(function_proposal_sourcecode.encode('utf8')).hexdigest()]}
+    transaction = {
+        'from': account.address,
+        'to': account.address,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 0,
+        'maxFeePerGas': 0,
+        'maxPriorityFeePerGas': 0,
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    print(tx_hash.hex())
---
+++ deploy_func_02_function_vote.py
@@ -0,0 +1,76 @@
+
+import hashlib
+import json
+
+import web3
+
+
+PROVIDER_HOST = 'http://127.0.0.1:8545'
+
+w3 = web3.Web3(web3.Web3.HTTPProvider(PROVIDER_HOST))
+
+
+function_vote_sourcecode = '''
+def function_vote(info, args):
+    assert args['f'] == 'function_vote'
+    sender = info['sender'].lower()
+    handle = handle_lookup(sender)
+    committee_members = set(get('committee', 'members', []))
+    assert handle in committee_members
+
+    fname = args['a'][0]
+    sourcecode_hexdigest = args['a'][1]
+    proposal = get('function', 'proposal', None, '%s:%s' % (fname, sourcecode_hexdigest))
+    votes = set(proposal['votes'])
+    votes.add(handle)
+    proposal['votes'] = list(votes)
+
+    if len(votes) >= len(committee_members)*2//3:
+        put(handle, 'function', 'code', proposal, fname)
+        global_state.put(('zen-code-function:%s' % (fname, )).encode('utf8'), proposal["sourcecode"].encode('utf8'))
+    else:
+        put(handle, 'function', 'proposal', proposal, '%s:%s' % (fname, sourcecode_hexdigest))
+'''
+
+
+if __name__ == '__main__':
+    account = setting.account
+    nonce = w3.eth.get_transaction_count(account.address)
+    print(account.address, nonce)
+
+    call = {"p": "minus",
+            "f": "function_proposal",
+            "a": ['function_vote', function_vote_sourcecode, [], [], []]}
+    transaction = {
+        'from': account.address,
+        'to': account.address,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 0,
+        'maxFeePerGas': 0,
+        'maxPriorityFeePerGas': 0,
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    print(tx_hash.hex())
+
+
+    call = {"p": "minus",
+            "f": "function_vote", 
+            "a": ['function_vote', hashlib.sha256(function_vote_sourcecode.encode('utf8')).hexdigest()]}
+    transaction = {
+        'from': account.address,
+        'to': account.address,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 0,
+        'maxFeePerGas': 0,
+        'maxPriorityFeePerGas': 0,
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    print(tx_hash.hex())
