bce8fe639cdb34c3c6a6dc5cc3dda7c6940649ed
KJ <kernel1983@gmail.com>
2025-06-20 16:27:47 -0400

    update

--- funcs/function_proposal.py
+++ funcs/function_proposal.py
@@ -3,6 +3,7 @@ def function_proposal(info, args):
     assert args['f'] == 'function_proposal'
     sender = info['sender']
     handle = handle_lookup(sender)
+    addr = handle or sender
     fname = args['a'][0]
     assert set(fname) <= set(string.ascii_lowercase+'_')
     sourcecode = args['a'][1]
@@ -27,4 +28,4 @@ def function_proposal(info, args):
 
     hexdigest = hashlib.sha256(sourcecode.encode('utf8')).hexdigest()
     k = 'function-proposal-%s:%s' % (fname, hexdigest)
-    put(handle, 'function', 'proposal', {'sourcecode': sourcecode, 'asset_permission': asset_permission, 'require': require, 'votes': []}, '%s:%s' % (fname, hexdigest))
+    put(addr, 'function', 'proposal', {'sourcecode': sourcecode, 'asset_permission': asset_permission, 'require': require, 'votes': []}, '%s:%s' % (fname, hexdigest))
--- funcs/function_vote.py
+++ funcs/function_vote.py
@@ -3,17 +3,18 @@ def function_vote(info, args):
     assert args['f'] == 'function_vote'
     sender = info['sender']
     handle = handle_lookup(sender)
+    addr = handle or sender
     committee_members = set(get('committee', 'members', []))
-    assert handle in committee_members
+    assert addr in committee_members
 
     fname = args['a'][0]
     sourcecode_hexdigest = args['a'][1]
     proposal = get('function', 'proposal', None, '%s:%s' % (fname, sourcecode_hexdigest))
     votes = set(proposal['votes'])
-    votes.add(handle)
+    votes.add(addr)
     proposal['votes'] = list(votes)
 
     if len(votes) >= len(committee_members)*2//3:
-        put(handle, 'function', 'code', proposal, fname)
+        put(addr, 'function', 'code', proposal, fname)
     else:
-        put(handle, 'function', 'proposal', proposal, '%s:%s' % (fname, sourcecode_hexdigest))
+        put(addr, 'function', 'proposal', proposal, '%s:%s' % (fname, sourcecode_hexdigest))
