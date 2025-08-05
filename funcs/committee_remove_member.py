443247a572f6216671b1031124965a4267cfc2a0
KJ <kernel1983@gmail.com>
2025-08-05 19:13:45 -0400

    update committee_add_member and committee_remove_member

--- funcs/committee_remove_member.py
+++ funcs/committee_remove_member.py
@@ -3,15 +3,17 @@ def committee_remove_member(info, args):
     assert args['f'] == 'committee_remove_member'
     sender = info['sender']
     handle = handle_lookup(sender)
+    addr = handle or sender
     committee_members = set(get('committee', 'members', []))
-    assert handle in committee_members
+    assert addr in committee_members
 
     user = args['a'][0]
-    votes = set(get('committee', 'proposal', [], user))
-    votes.add(handle)
+    votes = set(get('committee', 'proposal_remove', [], user))
+    votes.add(addr)
+    votes = list(votes)
 
     if len(votes) >= len(committee_members)*2//3:
         committee_members.remove(user)
-        put(handle, 'committee', 'members', list(committee_members))
-    else:
-        put(handle, 'committee', 'proposal', list(votes), user)
+        put(addr, 'committee', 'members', list(committee_members))
+        votes = None
+    put(addr, 'committee', 'proposal_remove', votes, user)
