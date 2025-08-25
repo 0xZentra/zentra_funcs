4ad67b5d37d4adc273b6b06dd282e82a90ddc41e
KJ <kernel1983@gmail.com>
2025-08-25 08:44:50 -0400

    orderbook

---
+++ testnet/deploy18_zip22_orderbook_proposal.py
@@ -0,0 +1,48 @@
+
+import hashlib
+import json
+import getpass
+import time
+
+import web3
+import eth_account
+
+import setting
+
+# PROVIDER_HOST = 'http://127.0.0.1:8545'
+PROVIDER_HOST = setting.rpc_url
+
+w3 = web3.Web3(web3.Web3.HTTPProvider(PROVIDER_HOST))
+
+ZEN_ADDR = '0x00000000000000000000000000000000007A656e'# hex of 'zen'
+
+sourcecode = open('../funcs/zip22.py', 'r').read()
+
+if __name__ == '__main__':
+    account = setting.account
+    nonce = w3.eth.get_transaction_count(account.address)
+    print(account.address, nonce)
+
+    call = {'p': setting.protocol,
+            'f': 'function_proposal',
+            'a': [['trade_limit_order', 'trade_market_order', 'trade_limit_order_cancel', 'trade_pair_create', 'trade_set_quote_token'], [hashlib.sha256(sourcecode.encode('utf8')).hexdigest()]]}
+    transaction = {
+        'from': account.address,
+        'to': ZEN_ADDR,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 40000,
+        'gasPrice': 1000000000,
+        # 'maxFeePerGas': 3000000000,
+        # 'maxPriorityFeePerGas': 0,
+        'chainId': setting.chain_id
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    # try:
+    tx_hash = w3.eth.send_raw_transaction(signed.raw_transaction)
+    print(tx_hash.hex())
+    # except Exception as e:
+    #     pass
+    time.sleep(5)
