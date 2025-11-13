8be662efe479bba78c7a95d142ae6f7fc03eabdc
KJ <kernel1983@gmail.com>
2025-11-12 20:23:28 -0500

    Add deploy scripts for mainnet

---
+++ mainnet/deploy7_zip2_function_vote.py
@@ -0,0 +1,44 @@
+
+import hashlib
+import json
+import getpass
+import time
+
+import web3
+import eth_account
+
+import setting
+
+PROVIDER_HOST = setting.rpc_url
+
+w3 = web3.Web3(web3.Web3.HTTPProvider(PROVIDER_HOST))
+
+ZEN_ADDR = '0x00000000000000000000000000000000007A656e'# hex of 'zen'
+
+if __name__ == '__main__':
+    account = setting.account
+    nonce = w3.eth.get_transaction_count(account.address)
+    print(account.address, nonce)
+
+    call = {'p': setting.protocol,
+            'f': "function_vote", 
+            'a': [2]}
+    transaction = {
+        'from': account.address,
+        'to': ZEN_ADDR,
+        'value': 0,
+        'nonce': w3.eth.get_transaction_count(account.address),
+        'data': json.dumps(call).encode('utf8'),
+        'gas': 22960,
+        'gasPrice': 1000000000,
+        # 'maxFeePerGas': 3000000000,
+        # 'maxPriorityFeePerGas': 0,
+        'chainId': setting.chain_id
+    }
+
+    signed = w3.eth.account.sign_transaction(transaction, account.key)
+    # try:
+    tx_hash = w3.eth.send_raw_transaction(signed.raw_transaction)
+    print(tx_hash.hex())
+    # except Exception as e:
+    #     pass
--- mainnet/setting.py
+++ mainnet/setting.py
@@ -10,6 +10,11 @@ import eth_account
 
 # account = eth_account.Account.from_key('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
 
+rpc_url = 'https://mainnet.base.org'
+chain_id = 8453
+chain = 'base'
+protocol = 'zentest'
+
 ps = getpass.getpass()
 js = open('../account.json', 'r').read()
 sk = eth_account.Account.decrypt(json.loads(js), ps)
